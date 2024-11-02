e4b8b621b8998dc92f86b898a6f62a83d5143b94
Suru-09 <dragos.surugiu13@yahoo.com>
2024-11-02 22:50:49 +0200

    Clear output on refresh

--- rlox-web/src/main.js
+++ rlox-web/src/main.js
@@ -3,6 +3,13 @@ import { createTextEditor } from '../resources/js/text-editor.js'
 import { appendRunButton } from '../resources/js/header.js'
 import init, {init_interpreter} from '../pkg/rlox_wasm.js'
 
+const clearOutput = () => {
+  const outputDoc = document.getElementById("output-id");
+  outputDoc.value = "";
+};
+
+window.onbeforeunload = clearOutput;
+
 init().then(() => {
   init_interpreter();
   createTextEditor();
